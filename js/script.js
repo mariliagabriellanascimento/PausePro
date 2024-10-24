7073807e0d7a806881e930af3591d327904a7839
mariliagabriellanascimento <marilila.gabi2000@gmail.com>
2024-10-24 14:11:17 -0300

    avancei

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+    const items = document.querySelectorAll('.carrossel-item');
+    const totalItems = items.length;
+
+    currentIndex = (currentIndex + direction + totalItems) % totalItems;
+
+    const offset = -currentIndex * 100; 
+    document.querySelector('.carousel-interno').style.transform = `translateX(${offset}%)`;
+}
