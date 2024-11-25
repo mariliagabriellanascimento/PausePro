be2ddddcaf33829546950c784c6ceb65a0c66ea9
mariliagabriellanascimento <marilila.gabi2000@gmail.con>
2024-11-25 14:10:49 -0300

    adiantado

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ function updateClock() {
 }
 setInterval(updateClock, 1000);
 
+//RELATORIOS
+
+document.querySelectorAll('.secao-relatorio h2').forEach(function(header) {
+    header.addEventListener('click', function() {
+        const table = header.nextElementSibling;
+
+        if (table.style.display === 'none' || table.style.display === '') {
+            table.style.display = 'block';
+        } else {
+            table.style.display = 'none';
+        }
+
+        const arrow = header.querySelector('span');
+        if (table.style.display === 'block') {
+            arrow.innerHTML = '&#9652';
+        } else {
+            arrow.innerHTML = '&#9662';
+        }
+    });
+});
