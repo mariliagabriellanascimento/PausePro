f53fc8f8882c7e3b396da971b05c5cc55e427de9
mariliagabriellanascimento <marilila.gabi2000@gmail.com>
2024-11-26 10:08:54 -0300

    continuar amanha

--- js/script.js
+++ js/script.js
@@ -8,8 +8,41 @@ function updateClock() {
 }
 setInterval(updateClock, 1000);
 
-//RELATORIOS
+//CARROSSEL LOGIN
+const carrosselLogin = document.querySelector(".carrossel-interno-login");
+const itensLogin = document.querySelectorAll(".item-carrossel-login");
+let indiceAtualCarrosselLogin = 0;
 
+function moverCarrosselLogin() {
+    indiceAtualCarrosselLogin++;
+
+    if (indiceAtualCarrosselLogin < 0) {
+        indiceAtualCarrosselLogin = itensLogin.length - 1;
+    } else if (indiceAtualCarrosselLogin >= itensLogin.length) {
+        indiceAtualCarrosselLogin = 0;
+    }
+
+    carrosselLogin.style.transform = `translateX(-${indiceAtualCarrosselLogin * 100}%)`;
+}
+
+//CARROSSEL HOME
+const carrosselHome = document.querySelector(".carrossel-interno-home");
+const itensHome = document.querySelectorAll(".item-carrossel-home");
+let indiceAtualCarrosselHome = 0;
+
+function moverCarrosselHome() {
+    indiceAtualCarrosselHome++;
+
+    if (indiceAtualCarrosselHome < 0) {
+        indiceAtualCarrosselHome = itensHome.length - 1;
+    } else if (indiceAtualCarrosselHome >= itensHome.length) {
+        indiceAtualCarrosselHome = 0;
+    }
+
+    carrosselHome.style.transform = `translateX(-${indiceAtualCarrosselHome * 100}%)`;
+}
+
+//RELATORIOS
 document.querySelectorAll('.secao-relatorio h2').forEach(function(header) {
     header.addEventListener('click', function() {
         const table = header.nextElementSibling;
